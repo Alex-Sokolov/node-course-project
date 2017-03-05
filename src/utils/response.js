1d806ad0b70b1e586128f6115ff9b317add07c26
Alex-Sokolov <Alex-Sokolov@users.noreply.github.com>
2017-03-05 16:24:54 +0300

    response.js убрана принудительная перекодировка в UTF8

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,13 +1,11 @@
-const ENCODING = 'utf8';
-
 /**
  * Формирование ответа для браузера
  */
 export default function generateResponse(data) {
   const response = [
     'HTTP/1.1 200 OK',
-    `Content-type: text/html; charset=${ENCODING}`,
-    data.toString(ENCODING),
+    // `Content-type: image/png`,
+    data,
     '',
   ].join('\r\n');
 
