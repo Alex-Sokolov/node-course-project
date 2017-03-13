20396a320a625bea02c0bb7896ab3c5fe68639af
Alex-Sokolov <Alex-Sokolov@users.noreply.github.com>
2017-03-13 22:23:27 +0300

    Добавлен класс HttpResponse

--- src/httpResponse.js
+++ src/httpResponse.js
@@ -1,1 +1,47 @@
 // Класс для работы с ответом
+import { Writable } from 'stream';
+
+/**
+ * Реализация нашего класса HttpResponse
+ */
+export default class HttpResponse extends Writable {
+  /**
+   * Конструктор класса
+   */
+  constructor(socket) {
+    super(socket);
+
+    if (!(this instanceof HttpResponse)) return new HttpResponse(socket);
+
+    console.log('[HttpResponse]');
+
+    // Socket
+    this.socket = socket;
+
+    // Флаг отправлены ли заголовки
+    this.headersSent = false;
+
+    return this;
+  }
+
+  // Установка заголовка
+  static setHeader() {
+    // устанавливаем заголовок
+  }
+
+  // Запись
+  static _write() {
+    // проверяем что отправлены заголовки
+    // если нет отправляем
+  }
+
+  // Отправка всех заголовков в socket
+  sendHeaders() {
+    if (this.headersSent === true) {
+      // заголовки уже отправлялись, выкидываем ошибку
+    }
+
+    // метод который отправляет все заголовки
+    this.headersSent = true;
+  }
+}
