692a0c0a7d651987708d928fb6b82f2078a14409
Alex-Sokolov <Alex-Sokolov@users.noreply.github.com>
2017-03-16 16:01:48 +0300

    (level 1) Работа с запросами через httpRequest

--- src/httpRequest.js
+++ src/httpRequest.js
@@ -1,5 +1,7 @@
 // Класс для работы с запросом
 import { Readable } from 'stream';
+import { EMPTY_LINE, convertBufferToStringArray } from './utils';
+import { parseStartString, parseHeaders } from './utils/parseHeaders';
 
 /**
  * Реализация нашего класса HttpRequest
@@ -13,26 +15,27 @@ export default class HttpRequest extends Readable {
 
     if (!(this instanceof HttpRequest)) return new HttpRequest(socket);
 
-    console.log('[HttpRequest]');
+    // Состояние запроса (headers/body)
+    this.state = 'HEADERS';
+
+    // Буфер для заголовков
+    this.buffer = Buffer.from([]);
 
     // Socket с данными запроса
     this.socket = socket;
+    this.socket.on('data', data => this.onData(data));
 
-    // Состояние запроса (headers/body)
-    this.state = 'HEADERS';
+    // Параметры запроса
+    this.headers = {};
+    this.method = '';
+    this.url = '';
 
     return this;
   }
 
   // Чтение запроса
   static _read() {
-    console.log('[HttpRequest _read]');
     this.socket.resume();
-  }
-
-  // Обработка заголовков
-  processHeaders() {
-    console.log('[HttpRequest processHeaders]', this.socket);
   }
 
   // Обработчик события data на socket
@@ -41,18 +44,58 @@ export default class HttpRequest extends Readable {
     if (this.state === 'BODY') {
       this.push(data);
       this.socket.pause();
+      return;
     }
 
     // Если получаем заголовки
+    // определяем есть ли пустая строка между заголовками и телом запроса
+    this.buffer = Buffer.concat([this.buffer, data]);
+    const emptyLineIndex = this.buffer.indexOf(Buffer.from(EMPTY_LINE));
 
-    // Если state headers то
-    // записываем в буфер
-    // если там встречается пустая строка
-    // если нет ждем следующее событие data
-    // если да то processHeaders
-    // unshift лишнего кусочка
-    // emit headers
-    // поменять состояние state = body
-    // socket.pause();
+    // Если нет пустой строки, ждём её дальше
+    if (emptyLineIndex === -1) {
+      this.socket.pause();
+      return;
+    }
+
+    // Если есть пустая строка
+
+    // Получаем кусочек буфера, что относится уже к телу запроса
+    const chunk = this.buffer.slice(emptyLineIndex + Buffer.from(EMPTY_LINE).length);
+    console.log('CHUNK LENGTH', chunk.length);
+
+    // В буфере оставляем только заголовки
+    this.buffer = this.buffer.slice(0, emptyLineIndex);
+    console.log('BUFFER LENGTH', this.buffer.length);
+
+    // Если был кусочек тела запроса, аншифтим его обратно
+    // this.socket.removeListener('readable', () => {});
+    if (chunk.length) {
+      console.log('NEED UNSHIFT CHUNK', chunk);
+      this.socket.unshift(chunk);
+    }
+
+    // Генерируем событие с заголовками
+    this.processHeaders();
+    this.emit('headers');
+
+    // Переключаемся в ожидание тела запроса
+    this.state = 'BODY';
+
+    this.socket.pause();
+  }
+
+  // Обработка заголовков
+  processHeaders() {
+    const [startString, ...headersArray] = convertBufferToStringArray(this.buffer);
+
+    // Парсинг стартовой строки запроса
+    const { method, url } = parseStartString(startString);
+    this.method = method;
+    this.url = url;
+
+    // Парсинг заголовков
+    const headers = parseHeaders(headersArray);
+    this.headers = headers;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import fs from 'fs-promise';
-import path from 'path';
+// import fs from 'fs-promise';
+// import path from 'path';
 import myHttp from './http';
 
 const server = myHttp.createServer();
-server.listen(process.env.PORT || 3000, () => {
-  console.log('[server listen]');
-});
+server.listen(process.env.PORT || 3000);
 
 server.on('request', (req, res) => {
-  console.log('[server request]');
   global.console.log(req.headers, req.method, req.url);
 
-  res.setHeader('Content-Type', 'application/json');
-  res.writeHead(200); // Вызов writeHead опционален
+  // global.console.log(req);
+  global.console.log(res);
 
-  // Вывод текстового файла
-  const STATIC_FOLDER = path.join(__dirname, '../static');
-  const filePath = path.join(STATIC_FOLDER, 'test.txt');
+  // res.setHeader('Content-Type', 'application/json');
+  // res.writeHead(200); // Вызов writeHead опционален
 
-  fs.createReadStream(filePath).then(stream => stream.pipe(res));
+  // // Вывод текстового файла
+  // const STATIC_FOLDER = path.join(__dirname, '../static');
+  // const filePath = path.join(STATIC_FOLDER, 'test.txt');
+
+  // fs.createReadStream(filePath).pipe(res);
 });
